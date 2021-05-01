d06d8ff0c396e804d7a3a70509d58c0a17fdc554
serajang99 <serajang1679@gmail.com>
2021-05-02 02:06:39 +0900

    fix merge conflict of #8 pr

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ import urllib.request
 import os
 
 # import makecsv
-
 credential_path = "/Users/jangseowoo/Downloads/stunning-yeti-312411-f2d6f0754d62.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 

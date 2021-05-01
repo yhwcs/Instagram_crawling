9783eab445c6713b87916e811e3b316b20c2712a
JungYeon Oh <ojy1369@naver.com>
2021-05-02 01:44:37 +0900

    0501 태그된 게시물 세기 -> 릴스 예외 처리 ok !!!!!!

---
+++ makecsv.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+from selenium.common.exceptions import NoSuchElementException
+from urllib.request import urlretrieve
+
+# import openpyxl
+import time
+import re
+import urllib.parse
+import urllib.request
+import os
+
+

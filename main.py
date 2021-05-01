124089094eac05b2c7326494ba69b93f9bf496a0
JungYeon Oh <ojy1369@naver.com>
2021-05-01 19:40:42 +0900

    0501 태그된 게시물 세기 -> 릴스 예외 처리 못함 (약간 수정)

--- main.py
+++ main.py
@@ -100,11 +100,10 @@ driver.get("https://www.instagram.com/accounts/login/")
 login_x_path = '/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[3]/button'
 
 # 개인정보 보안을 위한 수정 -> 계정을 하나 파자
-insta_id = 'ojy1369@naver.com' # input("인스타그램 아이디를 입력하세요 : ")
-insta_pw = 'here2Yellow%81' # input("인스타그램 비밀번호를 입력하세요 : ")
 
-#insta_id = 'myaho_123' # input("인스타그램 아이디를 입력하세요 : ")
-#insta_pw = 'capstonemyaho' # input("인스타그램 비밀번호를 입력하세요 : ")
+
+insta_id = 'myaho_123' # input("인스타그램 아이디를 입력하세요 : ")
+insta_pw = 'capstonemyaho' # input("인스타그램 비밀번호를 입력하세요 : ")
 driver.find_element_by_name('username').send_keys(insta_id)
 driver.find_element_by_name('password').send_keys(insta_pw)
 driver.find_element_by_xpath(login_x_path).click()
